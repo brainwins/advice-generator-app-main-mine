58a2384459e52ac2697368f4b83370603d490af3
unknown <shofondo@gmail.com>
2022-06-27 18:16:08 -0500

    all parts working

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
+button = document.querySelector('.dice');
+adviceBox = document.querySelector('.advice');
+adviceNumber = document.querySelector('.advice-number')
+
+button.addEventListener('click', async => {
+  FetchAdvice().then(advice => {
+    const adviceText = advice.slip.advice; // fetched movies
+    const adviceID = advice.slip.id;
+    adviceBox.textContent = adviceText;
+    adviceNumber.textContent = adviceID;
+  });
+})
+
+
 async function FetchAdvice(){
   const response = await fetch("https://api.adviceslip.com/advice");
   const advice = await response.json();
   return advice;
 }
-FetchAdvice().then(advice => {
-  console.log(advice.slip.advice); // fetched movies
-});
